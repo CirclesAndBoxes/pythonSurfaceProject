e2f42e9df0c4a0bb2b6ead2b63bf422cfb7abbf7
CirclesAndBoxes <joshua.lin.1281@gmail.com>
2024-08-27 22:33:06 -0500

    Ran program a few times, Changed requirements.txt

--- ris_folder/ris_load_model.py
+++ ris_folder/ris_load_model.py
@@ -1,4 +1,4 @@
-from stable_baselines3 import PPO, DDPG
+from stable_baselines3 import PPO, DDPG, A2C
 import os
 from ris_env import RisEnv
 from ris_env_const import RisEnvConst
@@ -7,9 +7,9 @@ from datetime import datetime
 
 # Following lines of code taken from website
 
-models_dir_past = f"models/1720497686"
-models_dir = f"models/1720497686_(2)"
-logdir = f"logs/1720497686_(2)-Log"
+models_dir_past = f"models/1723218553"
+models_dir = f"models/1723218553_(2)"
+logdir = f"logs/1723218553_(2)-Log"
 
 # Checks to see if the paths exist, otherwise makes the path
 if not os.path.exists(models_dir):
@@ -23,12 +23,12 @@ if not os.path.exists(logdir):
 env = RisEnvConst()
 env.reset()
 
-model = PPO.load(models_dir_past + "/1419000.zip", env=env, verbose=1, tensorboard_log=logdir)
-model.set_parameters(load_path_or_dict=(models_dir_past+"/1419000.zip"))
+model = A2C.load(models_dir_past + "/5360000.zip", env=env, verbose=1, tensorboard_log=logdir, seed=1)
+model.set_parameters(load_path_or_dict=(models_dir_past+"/5360000.zip"))
 # Will need to increase this... Since this kind of determines how long between each log.
-TIMESTEPS = 10000
+TIMESTEPS = 20000
 iters = 0
 for i in range(1, 10000):
     iters += 1
-    model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False, tb_log_name=f"PPO")
+    model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False, tb_log_name=f"A2C")
     model.save(f"{models_dir}/{TIMESTEPS*iters}")
